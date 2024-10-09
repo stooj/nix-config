b8e900b596987e6b66c593c902f403267a23c183
stoo johnston <stoo@pulumi.com>
2024-10-09 15:37:32 +0100

    enable flakes and new nix-command

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,7 @@
 
   time.timeZone = "Europe/Madrid";
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
