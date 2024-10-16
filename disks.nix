89aa14d07616ce680d427c431d23e85d0f66cfb2
stoo johnston <stoo@pulumi.com>
2024-10-16 20:33:42 +0100

    add /boot partition to disko

--- disks.nix
+++ disks.nix
@@ -6,6 +6,19 @@
         type = "disk";
         content = {
           type = "gpt";
+          partitions = {
+            efi = {
+              name = "efi";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "defaults" "umask=0077" ];
+              };
+            };
+          };
         };
       };
     };
