ea8879346f7c1bdb85470d77668dba4b59a3f1b9
stoo johnston <stoo@pulumi.com>
2024-10-16 20:22:08 +0100

    add partition table format for main disk

--- disks.nix
+++ disks.nix
@@ -4,6 +4,9 @@
       nvme = {
         device = "/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_500GB_S5NYNL0T600787F";
         type = "disk";
+        content = {
+          type = "gpt";
+        };
       };
     };
   };
