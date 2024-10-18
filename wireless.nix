98b1cc30169cbcf49d610ba98a467ae695264e6c
stoo johnston <stoo@pulumi.com>
2024-10-18 15:40:13 +0100

    generate wireless connection using nm2nix

--- wireless.nix
+++ wireless.nix
@@ -1,6 +1,31 @@
 {...}: {
   networking.networkmanager.ensureProfiles = {
     profiles = {
+      DIGIFIBRA-PLUS-YkPe = {
+        connection = {
+          id = "DIGIFIBRA-PLUS-YkPe";
+          interface-name = "wlp0s20f3";
+          type = "wifi";
+          uuid = "a7f75602-e5ce-464f-8b50-2e459b0be9a7";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "default";
+          method = "auto";
+        };
+        proxy = {};
+        wifi = {
+          mode = "infrastructure";
+          ssid = "DIGIFIBRA-PLUS-YkPe";
+        };
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-psk";
+          psk = "TODO: REPLACE ME WITH SECRET";
+        };
+      };
     };
   };
 }
