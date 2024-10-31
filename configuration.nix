d3dd6cfad6faefc6334c59bc3b9d9a8ef24f7205
stoo johnston <scj@stooj.org>
2024-10-31 15:16:28 +0100

    use vim as default editor

--- configuration.nix
+++ configuration.nix
@@ -71,8 +71,9 @@
   environment.systemPackages = with pkgs; [
     git
     gnupg
-    vim
   ];
+
+  programs.vim.defaultEditor = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
