62a69482cdf26fbfe5f4f12b5b05348ad8501623
stoo johnston <scj@stooj.org>
2024-10-31 18:03:44 +0100

    enable gpg and gpg-ssh

--- configuration.nix
+++ configuration.nix
@@ -70,18 +70,14 @@
 
   environment.systemPackages = with pkgs; [
     git
-    gnupg
   ];
 
   programs.vim.defaultEditor = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
