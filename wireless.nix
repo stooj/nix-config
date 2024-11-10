e7e66ac8dfc460d7d87cd0780169d9c8e9a418df
stoo johnston <scj@stooj.org>
2024-11-10 16:31:40 +0100

    add phone hotspot nmconnection

--- wireless.nix
+++ wireless.nix
@@ -1,13 +1,38 @@
 {config, ...}: {
   sops.secrets.rental-flat-psk = {};
+  sops.secrets.ceres-hotspot-psk = {};
   sops.templates."wireless.env".content = ''
     RENTAL_FLAT_PSK = "${config.sops.placeholder.rental-flat-psk}"
+    CERES_HOTSPOT_PSK = "${config.sops.placeholder.ceres-hotspot-psk}"
   '';
   networking.networkmanager.ensureProfiles = {
     environmentFiles = [
       config.sops.templates."wireless.env".path
     ];
     profiles = {
+      ceres-hotspot = {
+        connection = {
+          id = "ceres-hotspot";
+          type = "wifi";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "default";
+          method = "auto";
+        };
+        proxy = {};
+        wifi = {
+          mode = "infrastructure";
+          ssid = "ceres";
+        };
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-psk";
+          psk = "$CERES_HOTSPOT_PSK";
+        };
+      };
       rental-flat = {
         connection = {
           id = "rental-flat";
