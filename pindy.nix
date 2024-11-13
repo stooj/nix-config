2fb38856de47a3d521fe024f83f0344e271b5984
stoo johnston <scj@stooj.org>
2024-11-13 22:18:03 +0100

    let home manager install and manage itself

--- pindy.nix
+++ pindy.nix
@@ -3,5 +3,7 @@
   home.username = "pindy";
   home.homeDirectory = "/home/pindy";
 
+  programs.home-manager.enable = true;
+
   home.stateVersion = "24.05";
 }
--- stooj.nix
+++ stooj.nix
@@ -3,5 +3,7 @@
   home.username = "stooj";
   home.homeDirectory = "/home/stooj";
 
+  programs.home-manager.enable = true;
+
   home.stateVersion = "24.05";
 }
