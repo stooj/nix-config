3e6efda43d24e02783c4f40bff79b5733ff0310c
stoo johnston <scj@stooj.org>
2024-11-23 08:39:07 +0100

    remove color config for i3 bar

--- stooj.nix
+++ stooj.nix
@@ -47,36 +47,6 @@
             size = 8.0;
           };
           trayOutput = "primary";
-          colors = {
-            background = "#000000";
-            statusline = "#ffffff";
-            separator = "#666666";
-            focusedWorkspace = {
-              border = "#4c7899";
-              background = "#285577";
-              text = "#ffffff";
-            };
-            activeWorkspace = {
-              border = "#333333";
-              background = "#5f676a";
-              text = "#ffffff";
-            };
-            inactiveWorkspace = {
-              border = "#333333";
-              background = "#222222";
-              text = "#888888";
-            };
-            urgentWorkspace = {
-              border = "#2f343a";
-              background = "#900000";
-              text = "#ffffff";
-            };
-            bindingMode = {
-              border = "#2f343a";
-              background = "#900000";
-              text = "#ffffff";
-            };
-          };  
         }
       ];
       keybindings =
