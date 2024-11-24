4f1512664ac27f75268cae7b4d2fb2112fa8c164
stoo johnston <scj@stooj.org>
2024-11-24 20:50:02 +0100

    add stooj-dot-org blog to mr config

--- stooj.nix
+++ stooj.nix
@@ -24,6 +24,9 @@
   programs.mr = {
     enable = true;
     settings = {
+      "code/docs/stooj-dot-org" = {
+        checkout = "git clone 'git@github.com:stooj/stooj-dot-org.git'";
+      };
       "code/nix/nix-config" = {
         checkout = "git clone 'git@github.com:stooj/nix-config.git'";
       };
