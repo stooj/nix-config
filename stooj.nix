803221d1b0ea2c6f0e22226aa02ce5bf16a66f5c
stoo johnston <scj@stooj.org>
2024-11-24 20:16:13 +0100

    add search engine shortcuts for qutebrowser

--- stooj.nix
+++ stooj.nix
@@ -38,6 +38,23 @@
         "yv" = "yank inline [[{url}|{title}]]";
       };
     };
+    searchEngines = {
+      DEFAULT = "https://duckduckgo.com/?q={}";
+      archwiki = "https://wiki.archlinux.org/index.php?search={}";
+      bashfaq = "http://mywiki.wooledge.org/BashGuide?action=fullsearch&titlesearch=0&value={}&context=180";
+      define = "https://en.wiktionary.org/w/index.php?search={}";
+      duck = "https://duckduckgo.com/?q={}";
+      gmap = "http://maps.google.com/?q={}";
+      map = "https://www.openstreetmap.org/search?query={}";
+      man = "https://man.archlinux.org/search?q={}&go=Go";
+      nixoption = "https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={}";
+      nixpkg = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={}";
+      nixwiki = "https://nixos.wiki/index.php?search={}&go=Go";
+      pacman = "https://www.archlinux.org/packages/?q={}";
+      vimext = "https://vimawesome.com/?q={}";
+      wiki = "https://en.wikipedia.org/w/index.php?search={}";
+      youtube = "https://www.youtube.com/results?search_query={}";
+    };
   };
 
   programs.rofi = {
