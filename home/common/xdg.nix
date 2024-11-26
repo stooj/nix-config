c8f1142b56e363fd20deeda7938c6e72dab3182c
stoo johnston <scj@stooj.org>
2024-11-26 22:35:45 +0100

    create lowercased home directories

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -1,4 +1,20 @@
-{ ... }:
+{ config, ... }:
 {
-  xdg.enable = true;
+  xdg = {
+    enable = true;
+    userDirs = let
+      homeDir = config.home.homeDirectory;
+    in {
+      enable = true;
+      createDirectories = true;
+      desktop = "${homeDir}/desktop";
+      documents = "${homeDir}/documents";
+      download = "${homeDir}/downloads";
+      music = "${homeDir}/music";
+      pictures = "${homeDir}/pictures";
+      publicShare = "${homeDir}/public";
+      templates = "${homeDir}/templates";
+      videos = "${homeDir}/videos";
+    };
+  };
 }
