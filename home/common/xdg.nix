fd1fbd1b32db8bab02473f74a410428bdb8aea4b
stoo johnston <scj@stooj.org>
2024-11-26 21:34:15 +0100

    start defining directories

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ config, ... }:
 {
-  xdg.enable = true;
+  xdg = {
+    enable = true;
+    userDirs = let
+      homeDir = config.home.homeDirectory;
+    in {
+      enable = true;
+      createDirectories = true;
+      desktop = "${homeDir}/desktop";
+      documents = "${homeDir}/documents";
+    };
+  };
 }
