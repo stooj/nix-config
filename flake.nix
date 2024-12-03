01ef2427e132da2ac0bba04cb8767f4670d386fa
stoo johnston <scj@stooj.org>
2024-12-03 22:57:23 +0100

    switch inputs to 24.11 release

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
