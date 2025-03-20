8b930debab8a7443e52fd641c5aaddec8f06ce89
stoo johnston <scj@stooj.org>
2025-03-20 23:29:18 +0100

    auto-sync downloads directory

--- home/common/nextcloud.nix
+++ home/common/nextcloud.nix
@@ -1,1 +1,16 @@
-{...}:{}
+{pkgs, ...}:{
+  systemd.user = {
+    services.nextcloud-downloads-autosync = {
+      Unit = {
+        Description = "Auto sync Nextcloud downloads dir";
+        After = "network-online.target";
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd -h --user \${NEXTCLOUD_USERNAME} --password \${NEXTCLOUD_PASSWORD} --path /downloads %D/nextcloud/downloads \${NEXTCLOUD_URL}";
+        TimeoutStopSec = "180";
+      };
+      Install.WantedBy = ["multi-user.target"];
+    };
+  };
+}
