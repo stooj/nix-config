d4d854746c557eaadd41306107676dd764417800
stoo johnston <scj@stooj.org>
2025-04-05 07:56:21 +0200

    try to enable alder lake for drummer

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
             users.pindy = import ./home/pindy;
           };
         }
+        nixos-hardware.nixosModules.common-cpu-intel-alder-lake
         nixos-hardware.nixosModules.system76
         sops-nix.nixosModules.sops
         ./darp8.nix
