d61667f97e27a3a737603751560acd4b62a5f88f
stoo johnston <scj@stooj.org>
2025-04-08 23:59:22 +0200

    install vim with ansiesc plugin

--- vim.nix
+++ vim.nix
@@ -1,5 +1,15 @@
 {pkgs, ...}:
-{
+let
+  vim-ansi-esc = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-plugin-AnsiEsc";
+    src = pkgs.fetchFromGitHub {
+      owner = "powerman";
+      repo = "vim-plugin-AnsiEsc";
+      rev = "690f820d20b6e3a79ba20499874eb7333aa4ca5c";
+      sha256 = "sha256-N7UVzk/XUX76XcPHds+lLMZzO7gahj/9LIfof2BPThc=";
+    };
+  };
+in {
   environment.variables = {
     EDITOR = "vim";
   };
@@ -7,6 +17,10 @@
     ((vim_configurable.override { }).customize{
       name = "vim";
       vimrcConfig = {
+        packages.myplugins = with pkgs.vimPlugins; {
+          start = [ vim-ansi-esc ];
+          opt = [];
+        };
         customRC = ''
           syntax on
           set number
