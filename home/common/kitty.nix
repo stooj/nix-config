e5ac2d404a9bab4427c2abff1ff8f2ee853356a4
stoo johnston <scj@stooj.org>
2025-04-08 22:55:34 +0200

    use vim as scrollback buffer

--- home/common/kitty.nix
+++ home/common/kitty.nix
@@ -1,3 +1,8 @@
-{...}:{
-  programs.kitty.enable = true;
+{pkgs, ...}:{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      scrollback_pager = "${pkgs.vim}/bin/vim -c 'set ft=man' -";
+    };
+  };
 }
