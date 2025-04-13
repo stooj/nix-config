1d09e337d12ef764bb779b0ac8ba6172c8880ced
stoo johnston <scj@stooj.org>
2025-04-13 19:57:12 +0200

    yank urls to clipboard

--- home/stooj/kitty.nix
+++ home/stooj/kitty.nix
@@ -3,6 +3,7 @@
     enable = true;
     keybindings = {
       "ctrl+shift+p>h" = "kitten hints --type hash --program *";
+      "ctrl+shift+e" = "kitten hints --type url --program *";
     };
   };
 }
