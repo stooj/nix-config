7d697b95ff44ed054c6bad2eebe7c99799a033fb
stoo johnston <scj@stooj.org>
2025-04-13 14:45:05 +0200

    revert to default nixos config for vim

--- vim.nix
+++ vim.nix
@@ -1,31 +1,7 @@
 {pkgs, ...}:
-let
-  vim-ansi-esc = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-plugin-AnsiEsc";
-    src = pkgs.fetchFromGitHub {
-      owner = "powerman";
-      repo = "vim-plugin-AnsiEsc";
-      rev = "690f820d20b6e3a79ba20499874eb7333aa4ca5c";
-      sha256 = "sha256-N7UVzk/XUX76XcPHds+lLMZzO7gahj/9LIfof2BPThc=";
-    };
+{
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
   };
-in {
-  environment.variables = {
-    EDITOR = "vim";
-  };
-  environment.systemPackages = with pkgs; [
-    ((vim_configurable.override { }).customize{
-      name = "vim";
-      vimrcConfig = {
-        packages.myplugins = with pkgs.vimPlugins; {
-          start = [ vim-ansi-esc ];
-          opt = [];
-        };
-        customRC = ''
-          syntax on
-          set number
-        '';
-      };
-    })
-  ];
 }
