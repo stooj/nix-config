0cbdb119f0e72a72d3e2cd65420e747709a1a8d7
stoo johnston <scj@stooj.org>
2025-04-13 20:03:15 +0200

    add clipboard yank for files and paths

--- home/stooj/kitty.nix
+++ home/stooj/kitty.nix
@@ -4,6 +4,7 @@
     keybindings = {
       "ctrl+shift+p>h" = "kitten hints --type hash --program *";
       "ctrl+shift+e" = "kitten hints --type url --program *";
+      "ctrl+shift+p>f" = "kitten hints --type path --program *";
     };
   };
 }
