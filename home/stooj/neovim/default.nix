92e2b576f2dc4fa481191d580a1b7c10710804a7
stoo johnston <scj@stooj.org>
2025-04-19 22:54:42 +0200

    move neovim settings to dedicated file

--- home/stooj/neovim/default.nix
+++ home/stooj/neovim/default.nix
@@ -1,16 +1,9 @@
 {...}:{
   imports = [
+    # General settings
+    ./settings.nix
+
+    # Theming
     ./style.nix
   ];
-  programs.nixvim = {
-    enable = true;
-    defaultEditor = true;
-    vimdiffAlias = true;
-    vimAlias = true;
-
-    opts = {
-      number = true;
-      relativenumber = true;
-    };
-  };
 }
---
+++ home/stooj/neovim/settings.nix
@@ -0,0 +1,13 @@
+{...}:{
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    vimdiffAlias = true;
+    vimAlias = true;
+
+    opts = {
+      number = true;
+      relativenumber = true;
+    };
+  };
+}
