a33d82081e1f2afc9422b0a91b53fa41a1e68dae
stoo johnston <scj@stooj.org>
2025-04-21 09:24:18 +0200

    neovim: buffer source for cmp

--- home/stooj/neovim/plugins/completion/cmp.nix
+++ home/stooj/neovim/plugins/completion/cmp.nix
@@ -2,5 +2,10 @@
   # hrsh7th/nvim-cmp: A completion engine plugin for neovim written in Lua.
   # Completion sources are installed from external repositories and "sourced".
   # https://github.com/hrsh7th/nvim-cmp
-  programs.nixvim.plugins.cmp.enable = true;
+  programs.nixvim.plugins.cmp = {
+    enable = true;
+    settings.sources = [
+      { name = "buffer"; }
+    ];
+  };
 }
