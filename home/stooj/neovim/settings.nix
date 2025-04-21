7e60640bece3617f6c9dcc3b79cd32f7a584b471
stoo johnston <scj@stooj.org>
2025-04-21 21:21:11 +0200

    neovim: jump to previous cursor position on reopen

--- home/stooj/neovim/settings.nix
+++ home/stooj/neovim/settings.nix
@@ -9,5 +9,16 @@
       number = true;
       relativenumber = true;
     };
+
+    autoCmd = [
+      {
+	# This autocommand remembers the position of the curser when a file is
+	# closed, and navigate back to that position when the file is opened
+	# again.
+        command = ''if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif'';
+	event = [ "BufRead" ];
+	pattern = "*";
+      }
+    ];
   };
 }
