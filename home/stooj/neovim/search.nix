2afeb718e43a04d98f90f0cbb6c06b1059c25481
stoo johnston <scj@stooj.org>
2025-04-24 23:12:18 +0200

    neovim: ignore case when searching

--- home/stooj/neovim/search.nix
+++ home/stooj/neovim/search.nix
@@ -1,6 +1,9 @@
 {...}: {
   programs.nixvim = {
     opts = {
+      # Usually I want my search to be case-insensitive, so as long as the
+      # whole search string is lower case, then ignore the case.
+      ignorecase = true;
     };
   };
 }
