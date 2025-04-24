0133ad59f4f0ccb0d36d440edabdceb457b3bf08
stoo johnston <scj@stooj.org>
2025-04-24 23:19:35 +0200

    neovim: show command preview in split

--- home/stooj/neovim/search.nix
+++ home/stooj/neovim/search.nix
@@ -9,6 +9,9 @@
       # Make replacements default to global ones (all matches on a line).
       # To make it not-global, add a `/g` to the end of the replace command.
       gdefault = true;
+      # Show the effects of `:substitute`, `:smagic`, `:snomagic`, and user
+      # commands with the `command-preview` flag as you type in a split.
+      inccommand = "split";
     };
   };
 }
