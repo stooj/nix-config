06aa1191f7a04f073f7632885d699c92ff0e9b24
stoo johnston <scj@stooj.org>
2025-04-24 23:15:39 +0200

    neovim: enable smartcase searching

--- home/stooj/neovim/search.nix
+++ home/stooj/neovim/search.nix
@@ -4,6 +4,8 @@
       # Usually I want my search to be case-insensitive, so as long as the
       # whole search string is lower case, then ignore the case.
       ignorecase = true;
+      # If my search string contains a capital letter, make the search case-sensitive
+      smartcase = true;
     };
   };
 }
