e72b76a52e1c58f5539ee3aa6e92a6649ca438c6
stoo johnston <scj@stooj.org>
2025-04-25 22:54:49 +0200

    neovim: create command for disabling conform formatting

--- home/stooj/neovim/plugins/lsp/conform.nix
+++ home/stooj/neovim/plugins/lsp/conform.nix
@@ -2,5 +2,21 @@
 # conform.nvim
 # Lightweight yet powerful formatter plugin for Neovim
 # See https://github.com/stevearc/conform.nvim
-programs.nixvim.plugins.conform-nvim.enable = true;
-}
+  programs.nixvim.plugins.conform-nvim.enable = true;
+  programs.nixvim.userCommands = {
+    FormatDisable = {
+      bang = true;
+      desc = "Disable autoformat-on-save";
+      command.__raw = ''
+        function(args)
+          if args.bang then
+            -- FormatDisable! will disable formatting just for this buffer
+            vim.b.disable_autoformat = true
+          else
+            vim.g.disable_autoformat = true
+          end
+        end
+      '';
+    };
+  };
+ }
