4d33cdbb3c50a3696b5fd68402846b28a13d1e70
stoo johnston <scj@stooj.org>
2025-04-26 09:43:43 +0200

    neovim: add toggle check into format_on_save

--- home/stooj/neovim/plugins/lsp/conform.nix
+++ home/stooj/neovim/plugins/lsp/conform.nix
@@ -2,7 +2,20 @@
 # conform.nvim
 # Lightweight yet powerful formatter plugin for Neovim
 # See https://github.com/stevearc/conform.nvim
-  programs.nixvim.plugins.conform-nvim.enable = true;
+  programs.nixvim.plugins.conform-nvim = {
+    enable = true;
+    settings = {
+      format_on_save = ''
+        function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback "}, on_format
+        end
+      '';
+    };
+  };
   programs.nixvim.userCommands = {
     FormatDisable = {
       bang = true;
@@ -28,4 +41,4 @@
       '';
     };
   };
- }
+}
