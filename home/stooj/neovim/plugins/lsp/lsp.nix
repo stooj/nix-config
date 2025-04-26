7bb8c7243067e7c990557e35bed14907eb523aea
stoo johnston <scj@stooj.org>
2025-04-26 23:09:17 +0200

    neovim: verbose logging for marksman lsp server

--- home/stooj/neovim/plugins/lsp/lsp.nix
+++ home/stooj/neovim/plugins/lsp/lsp.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   programs.nixvim.plugins.lsp = {
     enable = true;
     servers.nil_ls.enable = true;
     servers.marksman.enable = true;
+    servers.marksman.cmd = [
+      "${lib.getExe pkgs.marksman}"
+      "server"
+      "-v 5"
+    ];
+
   };
 }
