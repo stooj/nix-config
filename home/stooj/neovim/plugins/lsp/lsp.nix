7e3fe7524c5a2846995d2d7578a40a0bc9b7ead5
stoo johnston <scj@stooj.org>
2025-04-27 21:12:49 +0200

    neovim: remove marksman debugging config

--- home/stooj/neovim/plugins/lsp/lsp.nix
+++ home/stooj/neovim/plugins/lsp/lsp.nix
@@ -1,14 +1,8 @@
-{ pkgs, lib, ... }:
+{ ... }:
 {
   programs.nixvim.plugins.lsp = {
     enable = true;
     servers.nil_ls.enable = true;
     servers.marksman.enable = true;
-    servers.marksman.cmd = [
-      "${lib.getExe pkgs.marksman}"
-      "server"
-      "-v 1"
-    ];
-    servers.marksman.extraOptions.offset_encoding = "utf-8";
   };
 }
