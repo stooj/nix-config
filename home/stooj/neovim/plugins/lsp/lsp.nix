31bcec2142c575dba1e622ade0e9bc8ad913c9ae
stoo johnston <scj@stooj.org>
2025-05-02 18:11:57 +0200

    neovim: try to configure harper-ls

--- home/stooj/neovim/plugins/lsp/lsp.nix
+++ home/stooj/neovim/plugins/lsp/lsp.nix
@@ -5,7 +5,16 @@
     servers = {
       nil_ls.enable = true;
       marksman.enable = true;
-      harper_ls.enable = true;
+      harper_ls = {
+        enable = true;
+        settings = {
+          "[\"harper-ls\"]" = {
+            linters = {
+              SpellCheck = false;
+            };
+          };
+        };
+      };
     };
   };
 }
