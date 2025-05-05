eaab32ee9badf7960039a6f7e5c5e6fa8d2f94c2
stoo johnston <scj@stooj.org>
2025-05-05 21:10:28 +0200

    get executable in nixvim package for neovide path

--- home/stooj/neovide.nix
+++ home/stooj/neovide.nix
@@ -1,9 +1,9 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   programs.neovide = {
     enable = true;
     settings = {
-      neovim-bin = config.programs.nixvim.build.package;
+      neovim-bin = lib.getExe config.programs.nixvim.build.package;
     };
   };
 }
