538e516a3986bc887d49ed913627b563f3844b23
stoo johnston <scj@stooj.org>
2025-05-06 09:16:50 +0200

    neovide-style cursor for kitty

--- home/stooj/kitty.nix
+++ home/stooj/kitty.nix
@@ -7,6 +7,9 @@
       "ctrl+shift+e" = "kitten hints --type url --program *";
       "ctrl+shift+p>f" = "kitten hints --type path --program *";
     };
+    settings = {
+      cursor_trail = 2;
+    };
     shellIntegration.enableBashIntegration = true;
 
     font.name = "Hurmit Nerd Font";
