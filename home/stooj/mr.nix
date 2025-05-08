700001f4ab7723ec0b35175c9887140582b82bf8
stoo johnston <scj@stooj.org>
2025-05-08 08:32:16 +0200

    clone pulumi-higara to correct directory

--- home/stooj/mr.nix
+++ home/stooj/mr.nix
@@ -9,7 +9,7 @@
         checkout = "git clone 'git@github.com:stooj/nix-config.git'";
       };
       "code/pulumi/higara" = {
-        checkout = "git clone 'git@github.com:stooj/pulumi-higara.git'";
+        checkout = "git clone 'git@github.com:stooj/pulumi-higara.git' higara";
       };
     };
   };
