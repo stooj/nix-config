d461ac378ac8a4b117879489314b4ec285d9531c
stoo johnston <scj@stooj.org>
2025-05-11 17:27:46 +0200

    duplicate neorg localopts for markdown

--- home/stooj/neovim/filetypes/markdown.nix
+++ home/stooj/neovim/filetypes/markdown.nix
@@ -1,1 +1,17 @@
-{ ... }: { }
+{ ... }:
+{
+  programs.nixvim = {
+    files = {
+      "ftplugin/markdown.lua" = {
+        localOpts = {
+          conceallevel = 2;
+          textwidth = 0;
+          wrap = true;
+          breakindent = true;
+          breakindentopt = "shift:2";
+          linebreak = true;
+        };
+      };
+    };
+  };
+}
