523b823d0feddeccf569ab4a82673814f774118e
stoo johnston <scj@stooj.org>
2025-05-13 09:02:14 +0200

    vi keybinds for newsboat

--- home/stooj/newsboat.nix
+++ home/stooj/newsboat.nix
@@ -4,5 +4,19 @@
     enable = true;
     autoReload = true;
     reloadTime = 30;
+    extraConfig = ''
+      # Unbind default keys that would conflict with vi-mode
+      unbind-key ENTER
+      unbind-key j
+      unbind-key k
+      unbind-key J
+      unbind-key K
+
+      # bind keys - vi style
+      bind-key j down
+      bind-key k up
+      bind-key l open
+      bind-key h quit
+    '';
   };
 }
