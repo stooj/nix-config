a4c91cc56d53dbea7117ce48667b136f3e222126
stoo johnston <scj@stooj.org>
2025-05-15 21:42:42 +0200

    include all.lua snippets in correct directory

--- home/stooj/neovim/plugins/snippets/luasnip.nix
+++ home/stooj/neovim/plugins/snippets/luasnip.nix
@@ -1,5 +1,6 @@
 { ... }:
 {
+  home.file.".config/nvim/snippets/all.lua".source = ../../files/snippets/all.lua;
   home.file.".config/nvim/snippets/markdown.lua".source = ../../files/snippets/markdown.lua;
   # Snippet Engine for Neovim written in Lua
   # See https://github.com/L3MON4D3/LuaSnip
