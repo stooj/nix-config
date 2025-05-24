e7f7e4fdd3beff652b3c650fb4ac2c565ebea597
stoo johnston <scj@stooj.org>
2025-05-25 00:13:51 +0200

    replace konsole alias

--- home/pindy/preferences.nix
+++ home/pindy/preferences.nix
@@ -2,9 +2,9 @@
 {
   systemd.user.sessionVariables = {
     # Set the default terminal emulator
-    TERMINAL = "${pkgs.konsole}/bin/konsole";
+    TERMINAL = "${pkgs.kdePackages.konsole}/bin/konsole";
   };
   home.sessionVariables = {
-    TERMINAL = "${pkgs.konsole}/bin/konsole";
+    TERMINAL = "${pkgs.kdePackages.konsole}/bin/konsole";
   };
 }
