0e647a6e56db248e5a63278a326069b00e891185
stoo johnston <scj@stooj.org>
2025-05-25 08:38:16 +0200

    wrap lsp config in single programs.nixvim block

--- home/stooj/neovim/plugins/lsp/lsp.nix
+++ home/stooj/neovim/plugins/lsp/lsp.nix
@@ -1,25 +1,27 @@
 { ... }:
 {
-  programs.nixvim.extraConfigLua = ''
-      require('lspconfig').harper_ls.setup {
-      settings = {
-        ["harper-ls"] = {
-          linters = {
-            sentence_capitalization = false,
-            spell_check = false
+  programs.nixvim = {
+    extraConfigLua = ''
+        require('lspconfig').harper_ls.setup {
+        settings = {
+          ["harper-ls"] = {
+            linters = {
+              sentence_capitalization = false,
+              spell_check = false
+            }
           }
         }
       }
-    }
-  '';
-  programs.nixvim.plugins.lsp = {
-    enable = true;
-    servers = {
-      harper_ls.enable = true;
-      marksman.enable = true;
-      nil_ls.enable = true;
-      superhtml.enable = true;
-      stylelint_lsp.enable = true;
+    '';
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        harper_ls.enable = true;
+        marksman.enable = true;
+        nil_ls.enable = true;
+        superhtml.enable = true;
+        stylelint_lsp.enable = true;
+      };
     };
   };
 }
