c3375c9ddaf626c393ebcae95179b3f78c8b2492
stoo johnston <scj@stooj.org>
2025-05-26 09:54:19 +0200

    test a minimal configuration

--- home/stooj/neovim/default.nix
+++ home/stooj/neovim/default.nix
@@ -1,45 +1,15 @@
 { ... }:
 {
-  imports = [
-    # General settings
-    ./indent.nix
-    ./keymaps.nix
-    ./search.nix
-    ./settings.nix
-
-    # Filetype-specific settings
-    ./filetypes/markdown.nix
-
-    # Gui settings
-    ./neovide.nix
-
-    # Theming
-    ./style/icons.nix
-    ./style/theme.nix
-
-    # Completion
-    ./plugins/completion/cmp.nix
-
-    # Editor plugins and configurations
-    ./plugins/editor/treesitter.nix
-
-    # Documentation
-    ./plugins/documentation/neorg.nix
-
-    # LSP and formatting
-    ./plugins/linting.nix
-    ./plugins/lsp/conform.nix
-    ./plugins/lsp/fidget.nix
-    ./plugins/lsp/lsp.nix
-
-    # Snippets
-    ./plugins/snippets/luasnip.nix
-
-    # Utils
-    ./plugins/utils/image.nix
-    ./plugins/utils/lastplace.nix
-    ./plugins/utils/mini.nix
-    ./plugins/utils/telescope.nix
-    ./plugins/utils/which-key.nix
-  ];
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+    };
+    plugins.image = {
+      enable = true;
+      settings.backend = "kitty";
+    };
+  };
 }
