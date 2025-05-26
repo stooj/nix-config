9c0d781c5da3270cf4c6b0d602a8f32de2e357a5
stoo johnston <scj@stooj.org>
2025-05-26 10:32:26 +0200

    reenable a bunch of stuff

--- home/stooj/neovim/default.nix
+++ home/stooj/neovim/default.nix
@@ -2,7 +2,20 @@
 {
   imports = [
     # General settings
+    ./indent.nix
+    ./keymaps.nix
+    ./search.nix
     ./settings.nix
+
+    # Filetype-specific settings
+    ./filetypes/markdown.nix
+
+    # Gui settings
+    ./neovide.nix
+
+    # Theming
+    ./style/icons.nix
+    ./style/theme.nix
 
     # Completion
     ./plugins/completion/cmp.nix
